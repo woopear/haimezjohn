ae842203bb2312fc454557233399d0be0b819d55
john_dev <johnhaimez314@gmail.com>
2022-04-18 22:28:32 +0200

    creation page private et widget public profil

---
+++ lib/models/profil/presentation/private/profil_page_private.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfilPagePrivate extends ConsumerStatefulWidget {
+  const ProfilPagePrivate({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ProfilPagePrivateState();
+}
+
+class _ProfilPagePrivateState extends ConsumerState<ProfilPagePrivate> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
