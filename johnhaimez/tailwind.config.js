f7bc867f318f24a865ef964a020c6e12648ed876
john_dev <johnhaimez314@gmail.com>
2022-06-01 13:47:25 +0200

    fixes #11 ; config tailwind

--- johnhaimez/tailwind.config.js
+++ johnhaimez/tailwind.config.js
@@ -8,7 +8,30 @@ module.exports = {
   ],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fc: "#E7F2FF",
+        fc2: "#B8D8FF",
+        fcb: "#9DC9FF",
+        fd: "#001A39",
+        fd2: "#002F69",
+        fdb: "#1A4982",
+        ic: "#ffffff",
+        id: "#000000",
+        ac: "#00FFFF",
+        hc: "#006FFF",
+      },
+      fontFamily: {
+        body: ["Prompt", "sans-serif"],
+      },
+      fontSize: {
+        "size-title-page": "4rem",
+        "size-title-section": "3rem",
+        "size-title-box": "2rem",
+        "size-title-form": "1.5rem",
+        "size-title-article": "1.8rem",
+      },
+    },
   },
   plugins: [],
 };
