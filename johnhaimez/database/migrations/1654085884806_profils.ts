204012ffab14581c3482749acc900876a3883ded
john_dev <johnhaimez314@gmail.com>
2022-06-01 14:54:02 +0200

    feat #12 ; creation des migrations database

---
+++ johnhaimez/database/migrations/1654085884806_profils.ts
@@ -0,0 +1,32 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class extends BaseSchema {
+  protected tableName = "profils";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.string("firstname");
+      table.string("lastname");
+      table.string("username");
+      table.string("address");
+      table.string("code_post");
+      table.string("city");
+      table.string("tel");
+      table.string("email");
+      table.string("title");
+      table.string("copyright");
+      table.text("description", "mediumtext");
+      table.integer("image_id");
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
---
+++ johnhaimez/database/migrations/1654085893525_images.ts
@@ -0,0 +1,24 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class extends BaseSchema {
+  protected tableName = "images";
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.string("name");
+      table.text("path", "mediumtext");
+      table.integer("hardskills_id");
+      table.integer("softskills_id");
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
