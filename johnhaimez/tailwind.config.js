2fb8b69ba57544abc428906bac447ca190cfa786
john_dev <johnhaimez314@gmail.com>
2022-06-01 11:06:42 +0200

    feat #10 ; install et config tailwind

---
+++ johnhaimez/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  purge: [
+    "./resources/views/**/*.edge",
+    "./resources/assets/ts/**/*.ts",
+    "./resources/assets/js/**/*.js",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
